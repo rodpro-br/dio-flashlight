a5e8c71a74f074d8f1875ad2b81cfe3c4570c27e
Rodrigo Pronunciati <rodpro@hotmail.com>
2022-06-06 09:38:55 -0300

    Implementação inicial

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import React, {useState, useEffect} from 'react';
 import {View, StyleSheet, Image, TouchableOpacity} from 'react-native';
 import Torch from 'react-native-torch';
